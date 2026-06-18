37998ca0db979e82bf681e4f69b501c8f7701946
hamzaaoudin-commits <hamzaa.oudin@gmail.com>
2026-06-18 10:55:30 +0200

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <>
       <World />
+      <div className="glow" />
       <div className="vignette" />
       <div className="grain" />
       <Stations />
